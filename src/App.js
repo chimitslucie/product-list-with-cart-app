c07effa32685f5fee73c16a311ce81c8c9d5466d
chimitslucie <chimitslucie@gmail.com>
2024-10-17 17:03:41 +0200

    add container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import Cart from "./Components/Cart";
 
 function App() {
   return (
     <div className="App">
+      <div className="main">
+        <div className="container">
+          <Cart />
+        </div>
+      </div>
     </div>
   );
 }
